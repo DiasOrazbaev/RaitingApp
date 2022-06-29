1b7268f2f3d402f3b994bbfd2fe83b80f227708f
Dias <orazbaevdev@gmail.com>
2022-06-29 23:38:33 +0600

    append button arrow logic

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,18 @@
-import styles from "Button.module.css";
+import styles from "./Button.module.css";
 import {ButtonProps} from "./Button.props";
 const classNames = require('classnames');
+import ArrowIcon from './Arrow.svg';
 
 export const Button = ( { appearance, arrow = 'none', children, className, ...props }:ButtonProps ): JSX.Element => {
     return (
-        <button className={classNames(appearance, className, arrow)}> {children} </button>
+        <button className={classNames(styles.button, className, {
+            [styles.primary]: appearance == 'primary',
+            [styles.ghost]: appearance == 'ghost'
+        })} {...props}> {children}  {arrow != "none" &&
+            <span className={classNames(styles.arrow, {
+                [styles.down]: arrow == 'down'
+            })}>
+                <ArrowIcon/>
+            </span>} </button>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import {Htag} from "../components";
+import {Button, Htag} from "../components";
 
 export default function Home() {
     return (
         <div>
             <Htag tag={'h1'}>Работаем</Htag>
+
+            <Button appearance={'primary'} >Купить</Button>
+            <Button appearance={'ghost'} arrow={'right'}>Больше отзывов</Button>
         </div>
     );
 }
